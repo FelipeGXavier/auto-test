97cd276027dc08717e342a41da3971c59d76abb0
felipegxavier <felipexavier20015@gmail.com>
2021-02-18 21:55:14 -0300

    Add Util class

---
+++ java/core/src/main/java/core/io/Util.java
@@ -0,0 +1,27 @@
+package core.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+public class Util {
+
+    public static String generateRandomFileName(String ext) {
+        return UUID.randomUUID().toString().replaceAll("-", "").toUpperCase().concat("." + ext);
+    }
+
+    public static File createTempFile(String ext) throws IOException {
+        var tmp = System.getProperty("java.io.tmpdir");
+        var filename = generateRandomFileName(ext);
+        var path = tmp + "/" + filename;
+        var file = new File(path);
+        if (file.createNewFile()) {
+            return file;
+        }
+        return null;
+    }
+
+    public static boolean removeTempFile(String path) {
+        return new File(path).delete();
+    }
+}
---
+++ java/core/src/test/java/unit/UtilTest.java
@@ -0,0 +1,24 @@
+package unit;
+
+import core.io.Util;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UtilTest {
+
+    @Test
+    @DisplayName("Test util class when manipulating temporary files")
+    public void testTempFileManagement() throws IOException {
+        var file = Util.createTempFile("txt");
+        assertNotNull(file);
+        assertTrue(file.exists());
+        assertTrue(file.canWrite());
+        assertTrue(file.canRead());
+        Util.removeTempFile(file.getPath());
+        assertFalse(file.exists());
+    }
+}
